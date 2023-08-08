19181c82d8fb97f72ea2066688005bfedea1bcea
Henrik Hofmeister <henrik@newdawn.dk>
2023-08-08 16:11:00 +0200

    fix: Enable dev info in production build (#51)
    fix: Tray icon in dark mode

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -7,6 +7,7 @@ import webpack from 'webpack';
 import TsconfigPathsPlugins from 'tsconfig-paths-webpack-plugin';
 import webpackPaths from './webpack.paths';
 import { dependencies as externals } from '../../release/app/package.json';
+import Path from "path";
 
 const configuration: webpack.Configuration = {
     externals: [...Object.keys(externals || {})],
@@ -38,6 +39,20 @@ const configuration: webpack.Configuration = {
             '.yml',
             '.yaml',
         ],
+        alias: {
+            'react': Path.resolve(webpackPaths.rootPath, './node_modules/react'),
+            'mobx': Path.resolve(webpackPaths.rootPath, './node_modules/mobx'),
+            'mobx-react': Path.resolve(webpackPaths.rootPath, './node_modules/mobx-react'),
+            'react-dom': Path.resolve(webpackPaths.rootPath, './node_modules/react-dom'),
+            'react-router-dom': Path.resolve(webpackPaths.rootPath, './node_modules/react-router-dom'),
+            '@kapeta/ui-web-utils': Path.resolve(webpackPaths.rootPath, './node_modules/@kapeta/ui-web-utils'),
+            '@kapeta/ui-web-types': Path.resolve(webpackPaths.rootPath, './node_modules/@kapeta/ui-web-types'),
+            '@kapeta/ui-web-context': Path.resolve(webpackPaths.rootPath, './node_modules/@kapeta/ui-web-context'),
+            '@kapeta/ui-web-components': Path.resolve(webpackPaths.rootPath, './node_modules/@kapeta/ui-web-components'),
+            '@kapeta/ui-web-plan-editor': Path.resolve(webpackPaths.rootPath, './node_modules/@kapeta/ui-web-plan-editor'),
+            '@mui/material': Path.resolve(webpackPaths.rootPath, './node_modules/@mui/material'),
+            '@mui/icons-material': Path.resolve(webpackPaths.rootPath, './node_modules/@mui/icons-material'),
+        },
         modules: [webpackPaths.srcPath, 'node_modules'],
         // There is no need to add aliases here, the paths in tsconfig get mirrored
         plugins: [new TsconfigPathsPlugins()],
