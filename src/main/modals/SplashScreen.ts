5b5cc4e87a6768cdee22736cc255a74c69ef23ba
Henrik Hofmeister <henrik@newdawn.dk>
2023-08-24 17:15:25 +0200

    feat: Download default providers tarball if no providers are available

--- src/main/modals/SplashScreen.ts
+++ src/main/modals/SplashScreen.ts
@@ -2,6 +2,14 @@ import { BrowserWindow } from 'electron';
 import { attachIPCListener, getPreloadScript, resolveHtmlPath } from '../helpers';
 import { ClusterService } from '../services/ClusterService';
 import { hasApp } from '@kapeta/nodejs-process';
+import { extract } from 'tar-stream';
+import FS from 'fs-extra';
+import Path from 'node:path';
+import request from 'request';
+import gunzip from 'gunzip-maybe';
+import ClusterConfiguration from '@kapeta/local-cluster-config';
+
+const DEFAULT_PROVIDERS_URL = 'https://storage.googleapis.com/kapeta-production-cdn/archives/default-providers.tar.gz';
 
 export class SplashScreen {
     private win: BrowserWindow | null = null;
@@ -114,7 +122,82 @@ export class SplashScreen {
 
     private async performTests(win: BrowserWindow) {
         this.setStatus({});
+        await this.checkForDefault();
         await Promise.allSettled([this.checkNpm(), this.startCluster(win)]);
+    }
+
+    private async checkForDefault() {
+        const definitions = ClusterConfiguration.getDefinitions();
+        if (definitions.length < 1) {
+            try {
+                await this.installDefaults();
+            } catch (e) {
+                console.warn('Failed to install defaults', e);
+            }
+        }
+    }
+
+    private async installDefaults() {
+        const repoBase = ClusterConfiguration.getRepositoryBasedir();
+
+        return new Promise<void>((resolve, reject) => {
+            const extractor = extract();
+            const dirCache = new Set<string>();
+            extractor.on('entry', async function (header, stream, next) {
+                if (header.type !== 'file') {
+                    stream.on('end', function () {
+                        next(); // ready for next entry
+                    });
+                    stream.resume(); // just auto drain the stream
+                    return;
+                }
+
+                try {
+                    const dirname = Path.join(repoBase, Path.dirname(header.name));
+                    if (!dirCache.has(dirname)) {
+                        let dirExists = false;
+                        try {
+                            await FS.stat(dirname);
+                            dirExists = true;
+                        } catch (e) {}
+                        if (!dirExists) {
+                            await FS.mkdirp(dirname);
+                        }
+                        dirCache.add(dirname);
+                    }
+                    const fileTarget = Path.join(repoBase, header.name);
+                    stream.on('error', (err) => {
+                        reject(err);
+                    });
+                    stream.on('end', next);
+
+                    stream.pipe(
+                        FS.createWriteStream(fileTarget, {
+                            mode: header.mode,
+                        })
+                    );
+                } catch (e) {
+                    reject(e);
+                }
+            });
+
+            extractor.on('finish', function () {
+                // all entries done - lets finalize it
+                console.log('Default providers installed');
+                resolve();
+            });
+
+            extractor.on('error', function (err) {
+                reject(err);
+            });
+
+            console.log('Downloading default providers from %s', DEFAULT_PROVIDERS_URL);
+            const response = request(DEFAULT_PROVIDERS_URL);
+            response.on('error', function (err) {
+                reject(err);
+            });
+            response.pipe(gunzip()).pipe(extractor);
+        });
     }
 
     close() {
