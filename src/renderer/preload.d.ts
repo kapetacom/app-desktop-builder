1b3685f11794762c7720b566bc6daf34fa009b20
Henrik Hofmeister <henrik@newdawn.dk>
2023-08-31 23:02:49 +0200

    feat: Add segment.com tracking

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -1,11 +1,61 @@
 import { ElectronHandler, KapetaDesktop } from 'main/preload';
 import { KapetaBrowserAPI } from './kapeta';
 
+interface PageProperties {
+    title?: string;
+    url?: string;
+    path?: string;
+    referrer?: string;
+    [key: string]: any;
+}
+
+interface IdentityTraits {
+    name?: string;
+    email?: string;
+    nickname?: string;
+    username?: string;
+    createdAt?: Date;
+    company?: {
+        id?: string;
+        name?: string;
+        industry?: string;
+        employee_count?: number;
+        plan?: string;
+    };
+    plan?: string;
+    [key: string]: any;
+}
+
+interface GroupTraits {
+    id?: string;
+    website?: string;
+    name?: string;
+    industry?: string;
+    createdAt?: Date;
+    employees?: number;
+    plan?: string;
+    'total billed'?: number;
+    [key: string]: any;
+}
+
+interface TrackProps {
+    revenue?: number;
+    value?: number;
+    currency?: string;
+    [key: string]: any;
+}
+
 declare global {
     interface Window {
         electron: ElectronHandler;
         Kapeta: KapetaBrowserAPI;
         KapetaDesktop: KapetaDesktop;
+        analytics: {
+            identify: (userId: string, traits?: IdentityTraits) => void;
+            track: (event: string, properties?: TrackProps) => void;
+            page: (name: string, properties?: PageProperties) => void;
+            group: (groupId: string, traits?: GroupTraits) => void;
+        };
     }
 }
 
