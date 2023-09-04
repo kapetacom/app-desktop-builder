baa155f27c834204b5ef7720310529c43c34d68b
Henrik Hofmeister <henrik@newdawn.dk>
2023-09-04 16:02:26 +0200

    fix: Form buttons not activated when they should (#106)
    fix: UI Glitch for resource buttons
    fix: Kill "local" namespace - we require being logged in

--- src/renderer/hooks/useNamespacesForField.ts
+++ src/renderer/hooks/useNamespacesForField.ts
@@ -2,8 +2,6 @@ import { useKapetaContext } from './contextHook';
 import { useMemo } from 'react';
 import { useFormContextField } from '@kapeta/ui-web-components';
 
-export const LOCAL_NAMESPACE = 'local';
-
 export const useNamespacesForField = (fieldName: string) => {
     const context = useKapetaContext();
     const formField = useFormContextField(fieldName);
@@ -12,13 +10,13 @@ export const useNamespacesForField = (fieldName: string) => {
         const fromValue: string[] = [];
         if (formField.get() && formField.get().includes('/')) {
             const existingHandle = formField.get().split('/')[0];
-            if (existingHandle !== LOCAL_NAMESPACE && existingHandle && !fromValue.includes(existingHandle)) {
+            if (existingHandle && !fromValue.includes(existingHandle)) {
                 fromValue.push(existingHandle);
             }
         }
 
         if (!identity) {
-            return [...fromValue, LOCAL_NAMESPACE];
+            return [...fromValue];
         }
 
         const memberships = context.contexts?.memberships ?? [];
@@ -26,7 +24,6 @@ export const useNamespacesForField = (fieldName: string) => {
             ...fromValue,
             identity.handle,
             ...memberships.map((membership) => membership.identity.handle),
-            'local',
         ]);
         return Array.from(out);
     }, [context.profile, context.contexts?.memberships, formField]);
