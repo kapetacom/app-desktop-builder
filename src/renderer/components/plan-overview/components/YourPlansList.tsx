60173538a20709060c72eb43027d2377f2277551
Jacob Wejendorp <jacob@wejendorp.dk>
2023-10-23 18:31:02 +0200

    feat: empty state launchpad graphics [KAP-1569]

--- src/renderer/components/plan-overview/components/YourPlansList.tsx
+++ src/renderer/components/plan-overview/components/YourPlansList.tsx
@@ -10,6 +10,7 @@ import { AssetInfo, AssetThumbnail } from '@kapeta/ui-web-plan-editor';
 import { useLoadedPlanContext } from '../../../utils/planContextLoader';
 import { installerService } from '../../../api/installerService';
 import { grey } from '@mui/material/colors';
+import { EmptyStateBox } from '@kapeta/ui-web-components';
 
 interface Props {
     plans: AssetInfo<Plan>[];
@@ -22,6 +23,79 @@ const YourPlansListInner = (props: Props) => {
     const kapetaContext = useKapetaContext();
 
     if (props.plans.length < 1) {
+        return (
+            <Stack direction="row" flexGrow={1} sx={{ maxWidth: '1152px' }}>
+                <Stack
+                    sx={{
+                        border: '1px solid #0000001f',
+                        borderRight: 0,
+                        alignItems: 'flex-start',
+                        p: 4,
+                        pt: 6,
+                        pb: 12,
+                        borderTopLeftRadius: '10px',
+                        borderBottomLeftRadius: '10px',
+                        flexBasis: '50%',
+                        maxWidth: '380px',
+                    }}
+                    gap={2}
+                >
+                    <Typography variant="h6">Your plans will appear here</Typography>
+                    <Typography
+                        sx={{
+                            '.MuiButtonBase-root': {
+                                padding: 0,
+                                minWidth: 0,
+                            },
+                        }}
+                    >
+                        You don't have any plans yet.
+                        <br />
+                        <Button variant="text" onClick={props.onPlanCreate}>
+                            Create
+                        </Button>
+                        <span>, </span>
+                        <Button variant="text" onClick={() => kapetaContext.blockHub.open()}>
+                            Find
+                        </Button>
+                        <span> or </span>
+                        <Button variant="text" onClick={props.onPlanImport}>
+                            Import
+                        </Button>
+                        <span> a new Plan to see it here.</span>
+                    </Typography>
+
+                    <Button variant="contained" color="primary" onClick={props.onPlanCreate}>
+                        Create new Plan
+                    </Button>
+                </Stack>
+
+                <EmptyStateBox
+                    size={170}
+                    icon="launchpad"
+                    title={
+                        <Button
+                            variant="text"
+                            component="a"
+                            href="https://docs.kapeta.com/docs/plans"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            More about plans
+                        </Button>
+                    }
+                    sx={{
+                        backgroundColor: '#f9f9f9',
+                        flexGrow: 1,
+                        borderRadius: '10px',
+                        borderBottomLeftRadius: 0,
+                        borderTopLeftRadius: 0,
+                        flexBasis: '50%',
+                    }}
+                />
+            </Stack>
+        );
+
         return (
             <Stack
                 sx={{
