a63e52394b9386f731ace52dd011499f17df2ae3
Henrik Hofmeister <henrik@newdawn.dk>
2023-10-28 12:20:00 +0200

    feat: Adds icon tips and data-kap-ids

--- src/renderer/components/plan-editor/panels/GatewaysList.tsx
+++ src/renderer/components/plan-editor/panels/GatewaysList.tsx
@@ -1,18 +1,17 @@
 import { GatewayCard, PlannerContext } from '@kapeta/ui-web-plan-editor';
-import { Badge, Chip, Typography } from '@mui/material';
-import { Box, Stack } from '@mui/system';
+import { Typography } from '@mui/material';
+import { Stack } from '@mui/system';
 import { withErrorBoundary } from 'react-error-boundary';
-import { Tooltip as KapTooltip } from '@kapeta/ui-web-components';
 import { useContext } from 'react';
 import { parseKapetaUri } from '@kapeta/nodejs-utils';
 import { BlockDefinition } from '@kapeta/schemas';
 import { InstanceStatus } from '@kapeta/ui-web-context';
-import { InfoOutlined } from '@mui/icons-material';
 import { BlockInstance } from '@kapeta/schemas';
 import { BlockInfo } from '../types';
 import { useEffect } from 'react';
 import { useAsyncRetry } from 'react-use';
 import { InstanceService } from '../../../api/InstanceService';
+import { TipIcon } from '@kapeta/ui-web-plan-editor/src/components/TipIcon';
 
 interface PlannerGatewaysListProps {
     /**
@@ -73,25 +72,23 @@ export const PlannerGatewaysList = withErrorBoundary(
 
         return (
             <Stack gap={3} sx={{ py: 1 }}>
-                <Stack gap={1.5} sx={{ py: 2 }}>
+                <Stack gap={1.5} sx={{ py: 2 }} data-kap-id={'plan-editor-gateways-public'}>
                     <Stack direction={'row'} alignItems={'center'} gap={1}>
                         <Typography variant={'h3'} fontSize={'14px'} fontWeight={700}>
                             Public URLs
+                            <TipIcon
+                                content={
+                                    <>
+                                        Public URLs for Gateways in your plan. Gateways will get a <b>kapeta.dev</b> URL
+                                        in cloud environments.
+                                        <br />
+                                        <br />
+                                        Public URLs can be configured for <b>custom domains in the gateway settings</b>.
+                                    </>
+                                }
+                                placement="top-start"
+                            />
                         </Typography>
-                        <KapTooltip
-                            title={
-                                <>
-                                    Public URLs for Gateways in your plan. Gateways will get a <b>kapeta.dev</b> URL in
-                                    cloud environments.
-                                    <br />
-                                    <br />
-                                    Public URLs can be configured for <b>custom domains in the gateway settings</b>.
-                                </>
-                            }
-                            placement="top-start"
-                        >
-                            <InfoOutlined fontSize={'small'} sx={{ ml: 0.5 }} />
-                        </KapTooltip>
                     </Stack>
 
                     {gateways
@@ -113,24 +110,22 @@ export const PlannerGatewaysList = withErrorBoundary(
                         })}
                 </Stack>
 
-                <Stack gap={1.5}>
+                <Stack gap={1.5} data-kap-id={'plan-editor-gateways-internal'}>
                     <Stack direction={'row'} alignItems={'center'} gap={1}>
                         <Typography variant={'h3'} fontSize={'14px'} fontWeight={700}>
                             Internal URLs
+                            <TipIcon
+                                content={
+                                    <>
+                                        Internal URLs will show for running blocks during local development, and are{' '}
+                                        <b>not available in cloud environments</b>. <br />
+                                        <br />
+                                        Internal URLs usually point to a local container port.
+                                    </>
+                                }
+                                placement="top-start"
+                            />
                         </Typography>
-                        <KapTooltip
-                            title={
-                                <>
-                                    Internal URLs will show for running blocks during local development, and are{' '}
-                                    <b>not available in cloud environments</b>. <br />
-                                    <br />
-                                    Internal URLs usually point to a local container port.
-                                </>
-                            }
-                            placement="top-start"
-                        >
-                            <InfoOutlined fontSize={'small'} sx={{ ml: 0.5 }} />
-                        </KapTooltip>
                     </Stack>
 
                     <Typography variant={'body2'}></Typography>
