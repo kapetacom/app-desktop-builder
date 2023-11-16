9a98fa1f9ea81e4e070f4344b0fb3ce1f4058d0c
Jacob Wejendorp <jacob@wejendorp.dk>
2023-11-16 12:29:21 +0100

    fix: only downgrade app when toggling off beta (#231)

--- src/main/main/AutoUpdateHelper.ts
+++ src/main/main/AutoUpdateHelper.ts
@@ -69,8 +69,8 @@ export class AutoUpdateHelper {
         // Github does not support release channels, so this is the best we've got
         // Prereleases will be any github release marked as prerelease
         autoUpdater.allowPrerelease = await this.getAllowPrerelease();
-        // allow downgrades for any pre-release version
-        autoUpdater.allowDowngrade = currentVersion.indexOf('-') > -1;
+        // allow downgrades for any pre-release version, in case we want to go back to stable
+        autoUpdater.allowDowngrade = currentVersion.indexOf('-') > -1 && !autoUpdater.allowPrerelease;
 
         let nextVersion: string | undefined;
         this.send(main, 'checking', initiatedByUser);
