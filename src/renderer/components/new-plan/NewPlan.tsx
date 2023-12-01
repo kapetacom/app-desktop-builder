945112245b623e51a81439c617cf4d3f8cdcf2e5
Jacob Wejendorp <jacob@wejendorp.dk>
2023-12-01 11:00:48 +0100

    wip: render a plan from AI!

--- src/renderer/components/new-plan/NewPlan.tsx
+++ src/renderer/components/new-plan/NewPlan.tsx
@@ -3,11 +3,30 @@ import { Paper } from '@mui/material';
 import { Box } from '@mui/system';
 import { AIBuilder } from './components/AIBuilder';
 import { useState } from 'react';
+import { DraftPlanView } from './components/DraftPlanView';
+import { useAsyncRetry } from 'react-use';
+import { aiService } from 'renderer/api/AIService';
 
-export interface NewPlanProps {}
+export interface NewPlanProps {
+    handle: string;
+}
 
 export const NewPlan = (props: NewPlanProps) => {
-    const {} = props;
+    const { handle = 'kapeta' } = props;
+
+    const testPlan = useAsyncRetry(async () => {
+        const response = await aiService.sendPrompt(
+            handle,
+            'An awesome demo plan with ponies and rainbows. Make sure the backend has a database.'
+        );
+
+        return {
+            plan: response.context.plan,
+            blocks: response.context.blocks,
+        };
+    }, []);
+
+    console.log(testPlan.value);
 
     const [createMode, setCreateMode] = useState<CreateMode>('ai');
 
@@ -47,7 +66,9 @@ export const NewPlan = (props: NewPlanProps) => {
                     flexGrow: 1,
                     zIndex: 1,
                 }}
-            ></Box>
+            >
+                <DraftPlanView plan={testPlan.value?.plan} blocks={testPlan.value?.blocks} />
+            </Box>
         </Box>
     );
 };
