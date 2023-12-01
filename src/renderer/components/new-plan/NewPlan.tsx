afe31da4a44d3103693177008235850ce9f4896b
Jacob Wejendorp <jacob@wejendorp.dk>
2023-12-01 16:05:41 +0100

    create plan and redirect

--- src/renderer/components/new-plan/NewPlan.tsx
+++ src/renderer/components/new-plan/NewPlan.tsx
@@ -11,17 +11,49 @@ import { useState } from 'react';
 import { DraftPlanView } from './components/DraftPlanView';
 import { BlockDefinition, Plan } from '@kapeta/schemas';
 import { useKapetaContext } from '../../hooks/contextHook';
+import { useNavigate } from 'react-router-dom';
+import { AssetService } from 'renderer/api/AssetService';
+import { FileSystemService } from 'renderer/api/FileSystemService';
 
 export const NewPlan = () => {
     const kapetaContext = useKapetaContext();
     const handle = kapetaContext.activeContext?.identity.handle;
 
-    const [createMode, setCreateMode] = useState<CreateMode>('ai');
-
     const [plan, setPlan] = useState<{ plan: Plan | undefined; blocks: BlockDefinition[] | undefined }>({
         plan: undefined,
         blocks: undefined,
     });
+
+    const [createMode, setCreateMode] = useState<CreateMode>('ai');
+    const onUsePlan = async () => {
+        if (!plan.plan || !plan.blocks) {
+            return;
+        }
+
+        const projectHome = await FileSystemService.getProjectFolder();
+        // Create blocks for plan
+        await Promise.all([
+            ...plan.blocks.map((block) =>
+                AssetService.create(`${projectHome}/${block.metadata.name}/kapeta.yml`, block).catch((err) => {
+                    if ((err.message as string).startsWith('File already exists')) {
+                        // everything is fine.
+                        return;
+                    }
+                    throw err;
+                })
+            ),
+        ]);
+
+        // Create plan
+        await AssetService.create(`${projectHome}/${plan.plan.metadata.name}/kapeta.yml`, plan.plan);
+
+        navigateToPlan(`${plan.plan.metadata.name}:local`);
+    };
+
+    const navigate = useNavigate();
+    const navigateToPlan = (ref: string) => {
+        navigate(`/edit/${encodeURIComponent(ref)}`);
+    };
 
     return (
         <Box
@@ -53,15 +85,7 @@ export const NewPlan = () => {
             </Paper>
 
             {/* Planner */}
-            <Box
-                sx={{
-                    backgroundColor: '#F6F1EE',
-                    flexGrow: 1,
-                    zIndex: 1,
-                }}
-            >
-                <DraftPlanView plan={plan.plan} blocks={plan.blocks} />
-            </Box>
+            <DraftPlanView plan={plan.plan} blocks={plan.blocks} onSubmit={onUsePlan} />
         </Box>
     );
 };
