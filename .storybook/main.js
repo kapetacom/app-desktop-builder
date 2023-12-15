764e54fcb5b8b4d924c936b5a894bf78de53783a
Jacob Wejendorp <jacob@wejendorp.dk>
2023-12-15 22:08:27 +0100

    build: fix react-router storybook issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,4 +3,15 @@ module.exports = {
     core: {
         builder: 'webpack5',
     },
+    webpackFinal: async (config, { configType }) => {
+        // Alias to prevent clashing with the react-router version in the release folder
+        // router context does not work when loading two different packages of react-router
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'react-router': require.resolve('react-router'),
+            'react-router-dom': require.resolve('react-router-dom'),
+        };
+
+        return config;
+    },
 };
