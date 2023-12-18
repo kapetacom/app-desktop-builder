e7e5514f479dd6146d52e126dd20af0eb9b72008
andertun <harald.andertun@gmail.com>
2023-12-18 22:23:51 +0100

    fix: Add license headers

--- src/main/services/SettingsService.ts
+++ src/main/services/SettingsService.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2023 Kapeta Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import request from 'request'; // Only used because it was already a dependency of the project
 import ClusterConfiguration from '@kapeta/local-cluster-config';
 import FS from 'fs-extra';
--- src/renderer/utils/AppSettingsContextSync.tsx
+++ src/renderer/utils/AppSettingsContextSync.tsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2023 Kapeta Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import { PropsWithChildren, createContext, useCallback, useEffect, useMemo, useState } from 'react';
 import { FileSystemService } from '../api/FileSystemService';
 import { DesktopAppSettings } from '../../main/services/SettingsService';
