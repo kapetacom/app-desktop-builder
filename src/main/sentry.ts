b0e5683521e8e285c5ea27b7f59feb9eb47c74b9
andertun <harald.andertun@gmail.com>
2024-01-04 14:46:34 +0100

    fix: Do not notify sentry about intercom errors

--- src/main/sentry.ts
+++ src/main/sentry.ts
@@ -10,4 +10,12 @@ Sentry.init({
     dsn: 'https://90b96576054385630d501d86133d372c@o4505820837249024.ingest.sentry.io/4505820839018496',
     enabled: process.env.NODE_ENV === 'production',
     release: `v${packageJson.version}`,
+    beforeSend(event) {
+        // If intercom was involved, don't send the event as it's not actionable
+        if (event.exception?.values?.[0]?.stacktrace?.frames?.some((frame) => frame.abs_path?.match(/intercom/i))) {
+            return null;
+        }
+
+        return event;
+    },
 });
